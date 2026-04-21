1ca170c02be103f2bb5605742f32d7ab122803c8
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-21 23:58:24 +0100

    Add DbHelper and clsDataAccessSettings with SQLite connection

---
+++ Repository/Factory/DbHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Repository.Factory
+{
+    public static class DbHelper
+    {
+        private static readonly string _cs = clsDataAccessSettings.ConnectionString;
+
+        public static SqliteConnection OpenConnection()
+        {
+            SqliteConnection con = new SqliteConnection(_cs);
+            con.Open();
+            using (SqliteCommand cmd = new SqliteCommand("PRAGMA foreign_keys = ON;", con))
+            {
+                cmd.ExecuteNonQuery();
+            }
+            return con;
+        }
+    }
+}
---
+++ Repository/Factory/clsDataAccessSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Repository.Factory
+{
+    public static class clsDataAccessSettings
+    {
+        public static string ConnectionString =
+            $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "CommercialStorePOS.db")};";
+    }
+}
