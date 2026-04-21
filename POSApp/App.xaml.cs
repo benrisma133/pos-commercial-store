4b41e4b36a9802ca72686ff076fcd8986ec1de97
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-21 22:58:41 +0100

    Initial commit - MAUI POS App (UI, Repository, Service)

---
+++ POSApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace POSApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
