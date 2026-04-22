ad269194609132347eaac9f143d3d84e374ed788
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-22 06:57:52 +0100

    Update Category model

--- Repository/Models/Category.cs
+++ Repository/Models/Category.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Repository.Models
+﻿namespace Repository.Models
 {
-    internal class Category
+    public class Category
     {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
