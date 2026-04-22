c8b1ff97f31711fd14f6c3ef462fd4c304929fc4
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-22 06:57:53 +0100

    Initialize database on app startup

--- POSApp/App.xaml.cs
+++ POSApp/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace POSApp
+﻿using Repository.Factory;
+
+namespace POSApp
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            Task.Run(async () => await DbInitializer.InitAsync()).Wait();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
